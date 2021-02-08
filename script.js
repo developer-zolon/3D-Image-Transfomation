fcfe4f54cf74849ccf1fad031d2945b7dfdc4655
developer-zolon <zolonchandro@gmail.com>
2021-02-08 13:19:30 +0600

    3D-Image-Transfomation
    JavaScript Project

---
+++ script.js
@@ -0,0 +1,20 @@
+// const card = document.querySelector('.card');
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+    card.addEventListener('mousemove', startRotate);
+    card.addEventListener('mouseout', stopRotate);
+});
+
+
+function startRotate(e) {
+    const cardItem = this.querySelector('.card-item');
+    //    cardItem.innerText = e.offsetX + ' ' + e.offsetY;
+    const halfHeight = cardItem.offsetHeight / 2;
+    const halfWidth = cardItem.offsetWidth / 2;
+    cardItem.style.transform = 'rotateX(' + -(e.offsetY - halfHeight) / 5 + 'deg) rotateY(' + (e.offsetX - halfWidth) / 5 + 'deg)'
+};
+
+function stopRotate() {
+    const cardItem = this.querySelector('.card-item');
+    cardItem.style.transform = 'rotate(0)';
+}
